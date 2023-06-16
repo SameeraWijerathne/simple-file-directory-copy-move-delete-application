5ad43778e7a9695d1ae17af4c422c05a5ca6aeeb
Sameera <sameerawncd@gmail.com>
2023-06-16 16:34:28 +0530

    button browse destination folder impelemted

--- src/main/java/lk/wsrp/sameera/controller/MainViewController.java
+++ src/main/java/lk/wsrp/sameera/controller/MainViewController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 
 import javax.swing.*;
 import java.io.File;
@@ -50,35 +51,26 @@ public class MainViewController {
     }
 
     @FXML
-    void btnCopyOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnDeleteOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnDestinationBrowseOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnMoveOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
     void btnSourceBrowseOnAction(ActionEvent event) {
         JFileChooser chooser = new JFileChooser();
         chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setDialogTitle("Select a file or directory");
         chooser.showOpenDialog(null);
         sourceFile = chooser.getSelectedFile();
         enableButtons();
         if (sourceFile == null) return;
         txtSource.setText(chooser.getSelectedFile().toString());
         System.out.println(sourceFile.getParentFile().length());
+    }
+
+    @FXML
+    void btnDestinationBrowseOnAction(ActionEvent event) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select the destination folder");
+        targetFolder = directoryChooser.showDialog(btnDestinationBrowse.getScene().getWindow());
+        enableButtons();
+        if (targetFolder == null) return;
+        txtDestination.setText(targetFolder.getAbsolutePath());
     }
 
     private void enableButtons() {
@@ -90,4 +82,18 @@ public class MainViewController {
         btnDelete.setDisable(sourceFile == null);
     }
 
+    @FXML
+    void btnCopyOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMoveOnAction(ActionEvent event) {
+
+    }
 }
