69fe7b8eb35ed10b1a862c69fdb815761bed990f
Sameera <sameerawncd@gmail.com>
2023-06-16 10:36:55 +0530

    initial commit with project setup

---
+++ src/main/java/lk/wsrp/sameera/AppInitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.wsrp.sameera;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/java/lk/wsrp/sameera/controller/MainViewController.java
@@ -0,0 +1,4 @@
+package lk.wsrp.sameera.controller;
+
+public class MainViewController {
+}
